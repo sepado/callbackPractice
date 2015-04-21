7a77bf9810aa9dbc2a489fe61d60dc29cbe755e5
sepado <72sepado@gmail.com>
2015-04-20 21:35:24 -0600

    Complete Day 9 Practice - CallBacks

--- callbackPractice.js
+++ callbackPractice.js
@@ -1,4 +1,5 @@
-/* In this repo your job is to write functions to make each function call work properly.
+/* In this repo your job is to write functions to make each function call 
+work properly.
 Below is a sample problem 
 
   //code here for sayHi
@@ -8,7 +9,8 @@ Below is a sample problem
    });
    
 
-and what you should write is the favNum function that makes the code above work, 
+and what you should write is the favNum function that makes the code 
+above work, 
     
     
    var sayHi = function(str, cb){
@@ -22,59 +24,67 @@ and what you should write is the favNum function that makes the code above work,
     
 */
 
-
-
-  //Code Here for first
+//Code Here for first
   
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+
+function first(array, callBack) {
+  var firstName = array[0];
+  callBack(firstName);
+}
+
 first(names, function(firstName){
   console.log('The first name in names is ', firstName)
 });
 
 
-
 /* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
-
-
-
-
-  //Code Here for last
+//Code Here for last
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+
+function last(array, callBack) {
+  var lastName = array[array.length - 1];
+  callBack(lastName);
+}
+
 last(names, function(lastName){
   console.log('The last name in names is ', lastName);
 });
 
 
-
-
-
 /* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
+//Code Here for multiply
 
-
-
-
-
-
-  //Code Here for multiply
+function multiply(a, b, callBack) {
+  var answer = a * b;
+  callBack(answer);
+}
 
 multiply(4, 3, function(answer){
   console.log('The answer is ', answer); //should console.log 12
 })
 
 
-
-
-
 /* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
-
-
-
-
-
-  //Code Here for contains
+//Code Here for contains
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+
+function contains(arr1, str1, callBack) {
+  var result = false;                           // set initial boolean value of false (to get else statement working)
+  for (var i = 0; i < arr1.length; i++) {
+    if (str1 === arr1[i]) {
+      result = true;                            // only sets true if str1 is found
+      callBack(result);
+      break;
+    }
+  }
+  if (result === false) {                       // stays at false if str1 is not found in arr1
+    callBack(result);
+  }
+}
+
 contains(names, 'Colt', function(result){
   if(result === true){
     console.log('Colt is in the array');
@@ -84,48 +94,57 @@ contains(names, 'Colt', function(result){
 });
 
 
-
-
-
 /* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
-
-
-
-
-    //Code Here for uniq
+//Code Here for uniq
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+
+//create a new empty array, nested for loops (for i searches old array, sets defalut boolean to false) 
+// (for j searches new array, if it finds the same value already there, it sets boolean to true)
+// (the bottom of for i checks to see if it was found, if not, it pushes name into the new array form the old)
+function uniq(arr1, callBack) {
+  var uniqArr = [];
+  for (var i = 0; i < arr1.length; i++) {
+    var found = false;
+    for (var j = 0; j < uniqArr.length; j++) {
+      if(arr1[i] === uniqArr[j]) {
+        found = true;
+        break;
+      }
+    } 
+    if(found === false) {
+      uniqArr.push(arr1[i]);
+    }  
+  }
+  callBack(uniqArr);
+}
+
 uniq(names, function(uniqArr){
   console.log('The new names array with all the duplicate items removed is ', uniqArr);
 });
 
 
-
-
-
 /* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
-
-
-
-
-    //Code Here for each
+//Code Here for each
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+
+// two callBack arguements - add var to each item in the array
+function each(arr1, callBack) {
+  for (var i = 0; i < arr1.length; i++) {
+    var item = arr1[i];
+    var indice = i;
+    callBack(item, indice);
+  }
+}
+
 each(names, function(item, indice){
   console.log('The item in the ' + indice + ' position is ' + item)
 });
 
 
-
-
-
 /* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
-
-
-
-
-
- //code here for getUserById
+//code here for getUserById
 
 var users = [
   {
@@ -148,6 +167,25 @@ var users = [
   },
 ];
 
+// passing single array, a string, and 1 callback arguement is req...
+// finds object by id, assigns var to user and passes back users[key]
+// the function call adds the other half users[key].email
+function getUserById(arr1, str1, callBack) {
+  for (key in arr1) {
+    if(arr1[key].id === str1) {
+      var user = arr1[key];
+      callBack(user);
+    }
+  }
+}
+
 getUserById(users, '16t', function(user){
   console.log('The user with the id 16t has the email of ' + user.email + ' the name of ' + user.name + ' and the address of ' + user.address); 
 });
+
+
+
+
+
+
+
